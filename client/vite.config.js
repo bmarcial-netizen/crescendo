73703e4dd80f0b83453e9bc6a99d52e0aef3f7ae
bmarcial <bmarcial@andrew.cmu.edu>
2026-02-20 03:36:55 -0500

    fixed my google shi

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env from project root (parent dir) so we can read GOOGLE_CLIENT_ID
-  const rootEnv = loadEnv(mode, '..', '')
+  // Use absolute path and empty prefix '' to load ALL env vars (not just VITE_*)
+  const rootEnv = loadEnv(mode, path.resolve(__dirname, '..'), '')
 
   return {
     plugins: [react()],
